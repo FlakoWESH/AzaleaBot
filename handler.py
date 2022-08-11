8e0789d5efacf91ae3b1aa1b876e290e4347aca0
spica <spicaofsky@gmail.com>
2022-08-10 20:04:59 -0600

    limit return number to 3

--- handler.py
+++ handler.py
@@ -1,9 +1,16 @@
+import random
+
 hasInit=False
 import util.findSticker
+
+STICKER_LIMIT_NUMBER=3
 
 def getStickers(keyword):
     global hasInit
     if not hasInit:
         util.findSticker.init()
         hasInit=True
-    return util.findSticker.lookupInDB(keyword)
+    result=util.findSticker.lookupInDB(keyword)
+    if len(result) > 3:
+        result=random.choices(result,k=STICKER_LIMIT_NUMBER)
+    return result
